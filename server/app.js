3eea0931ab02a4d87529859fded8e101dbe4d1ab
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-03 15:22:33 +0545

    first commit

---
+++ server/app.js
@@ -0,0 +1,30 @@
+//Modules
+require("dotenv").config(); // 👈 load env firstn
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//API Test
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        `Server is running on port http://localhost:${process.env.PORT || 5000}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB Error:", err);
+  });
